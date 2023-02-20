805fc1c81bed30be801f16f3feb8b770d221cff8
ahren <205530972@qq.com>
2023-02-20 14:11:31 +0800

    双向map 测试用例调整

--- Stl/map_test.go
+++ Stl/map_test.go
@@ -1,23 +1,43 @@
 package Stl
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestDoubleMap(t *testing.T) {
 	data := NewDoubleMap()
-	count := 10000
+	count := 100000
 	for i := 0; i < count; i++ {
 		data.Add(i, i*2)
 	}
 
-	for i := 0; i < count; i++ {
-		if value := data.GetValue(i); value == nil {
-			t.Errorf("获取value数据失败, key = %v", i)
-		}
-	}
+	var sign sync.WaitGroup
+	sign.Add(3)
 
-	for i := 0; i < count; i++ {
-		if value := data.GetKey(i * 2); value == nil {
-			t.Errorf("获取key数据失败, key = %v", i)
+	go func() {
+		for i := count; i < count*2; i++ {
+			data.Add(i, i*2)
 		}
-	}
+		sign.Done()
+	}()
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if value := data.GetValue(i); value == nil {
+				t.Errorf("获取value数据失败, key = %v", i)
+			}
+		}
+		sign.Done()
+	}()
+
+	go func() {
+		for i := 0; i < count; i++ {
+			if value := data.GetKey(i * 2); value == nil {
+				t.Errorf("获取key数据失败, key = %v", i)
+			}
+		}
+		sign.Done()
+	}()
+	sign.Wait()
 }
