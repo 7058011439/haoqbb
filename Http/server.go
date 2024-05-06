41ed30e36f52524ae9292c9d7e3c5771659a97c4
ahren <205530972@qq.com>
2024-05-06 15:14:54 +0800

    1.http新增请求耗时统计功能

--- Http/server.go
+++ Http/server.go
@@ -2,8 +2,11 @@ package Http
 
 import (
 	"fmt"
+	"github.com/7058011439/haoqbb/Log"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type ServerMode = string
@@ -12,6 +15,9 @@ const (
 	ServerModeRelease ServerMode = "release"
 	ServerModeDebug   ServerMode = "debug"
 	ServerModeTest    ServerMode = "test"
+
+	uristatus      = "/status"
+	uriresetstatus = "/resetstatus"
 )
 
 type Server struct {
@@ -49,8 +55,90 @@ func NewHttpServer(mode ServerMode) *Server {
 	if mode != ServerModeRelease {
 		en.Use(gin.Logger())
 	}
-	en.Use(gin.Recovery(), options)
+	en.Use(gin.Recovery(), statusMiddleware, options)
+	en.POST(uristatus, status)
+	en.POST(uriresetstatus, resetStatus)
 	return &Server{Engine: en}
+}
+
+type requestStatus struct {
+	requestTimes int64   // 总请求次数
+	costTime     float64 // 总处理时间
+	maxTime      float64 // 最长耗时
+	mutex        sync.Mutex
+}
+
+func (s *requestStatus) updateRequestData(t float64) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.requestTimes++
+	s.costTime += t
+	if t > s.maxTime {
+		s.maxTime = t
+	}
+}
+
+func (s *requestStatus) toMap() map[string]interface{} {
+	averageTime := 0.0
+	if s.requestTimes > 0 {
+		averageTime = s.costTime / float64(s.requestTimes)
+	}
+	return map[string]interface{}{
+		"request times    ": s.requestTimes,
+		"total cost time  ": fmt.Sprintf("%.3fs", s.costTime),
+		"max cost time    ": fmt.Sprintf("%.3fs", s.maxTime),
+		"average cost time": fmt.Sprintf("%.3fs", averageTime),
+	}
+}
+
+var allStatus sync.Map
+
+func statusMiddleware(c *gin.Context) {
+	start := time.Now()
+	c.Next()
+	elapsed := time.Since(start)
+	if data, ok := allStatus.Load(c.FullPath()); ok {
+		data.(*requestStatus).updateRequestData(elapsed.Seconds())
+	} else {
+		s := &requestStatus{}
+		s.updateRequestData(elapsed.Seconds())
+		allStatus.Store(c.FullPath(), s)
+	}
+}
+
+func status(c *gin.Context) {
+	request := struct {
+		URI string `json:"uri" form:"uri"`
+	}{}
+	if err := c.ShouldBind(&request); err == nil {
+		if request.URI == "" {
+			retData := map[string]interface{}{}
+			allStatus.Range(func(key, value interface{}) bool {
+				if key.(string) != uristatus && key.(string) != uriresetstatus {
+					retData[key.(string)] = value.(*requestStatus).toMap()
+				}
+				return true
+			})
+			c.JSON(200, retData)
+		} else {
+			if data, ok := allStatus.Load(request.URI); ok {
+				c.JSON(200, data.(*requestStatus).toMap())
+			} else {
+				c.JSON(200, (&requestStatus{}).toMap())
+			}
+		}
+	} else {
+		c.JSON(http.StatusOK, &WebResult{
+			Msg:  err.Error(),
+			Code: http.StatusInternalServerError,
+		})
+		Log.ErrorLog(err.Error())
+	}
+}
+
+func resetStatus(c *gin.Context) {
+	allStatus = sync.Map{}
+	c.JSON(200, "ok")
 }
 
 func options(c *gin.Context) {
