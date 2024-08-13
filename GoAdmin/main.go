67d5187bce6fc3ec546f124647fe8157cb01721d
ahren <205530972@qq.com>
2024-08-13 11:06:03 +0800

    1.goAdmin 模块新增swagger注释

--- GoAdmin/main.go
+++ GoAdmin/main.go
@@ -3,12 +3,13 @@ package GoAdmin
 import (
 	"fmt"
 	"github.com/7058011439/haoqbb/GoAdmin/config"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-
+	_ "github.com/7058011439/haoqbb/GoAdmin/docs"
 	_ "github.com/7058011439/haoqbb/GoAdmin/http/admin"
 	"github.com/7058011439/haoqbb/GoAdmin/http/common"
+	"github.com/7058011439/haoqbb/Http"
 	"github.com/7058011439/haoqbb/Log"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // @title         好奇宝宝后台
@@ -19,9 +20,12 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 func init() {
-	common.ServerAdmin.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	RegeditSwagger(common.ServerAdmin, "core", config.HttpPort())
 	common.ServerAdmin.Start(config.HttpPort())
-	url := fmt.Sprintf("http://localhost:%v/swagger/index.html#", config.HttpPort())
-	Log.Debug(fmt.Sprintf("打开:%v,可进入API调试", url))
 	Log.Log("基础服务启动完成")
+}
+
+func RegeditSwagger(server *Http.Server, path string, port int) {
+	server.GET(fmt.Sprintf("/swagger/%v/*any", path), ginSwagger.WrapHandler(swaggerFiles.NewHandler(), ginSwagger.InstanceName(path)))
+	Log.Debug(fmt.Sprintf("打开:http://localhost:%v/%v/index.html#,可进入API调试", port, path))
 }
