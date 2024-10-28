8941cdd7cd8f93633d6a6d518fb6fbba9691072d
ahren <205530972@qq.com>
2024-10-28 16:57:23 +0800

    1.协程池模块测试用例优化

--- Util/goroutine_pool_test.go
+++ Util/goroutine_pool_test.go
@@ -1,6 +1,7 @@
 package Util
 
 import (
+	"encoding/json"
 	"fmt"
 	"sync/atomic"
 	"testing"
@@ -8,16 +9,25 @@ import (
 )
 
 /*
-测试效果:
-1.通过协程池方式运算耗时33.61s, 运算结果=357913941332992000
-2.通过普通方式运算耗时265.51s, 运算结果=357913941332992000
-3.本测试环境为8核16线程CPU, 采用协程池可以将cpu利用率拉满
+测试效果本测试环境为8核16线程CPU:
+一、测试calcSum(密集计算型), 协程池优势明显
+1.通过协程池方式耗时33.61s, 运算结果=357913941332992000
+2.通过普通方式耗时265.51s, 运算结果=357913941332992000
+
+二、测试json.UmMarshal(耗时操作型), 协程池有一定优势
+1.通过协程池方式耗时5.62s
+2.通过普通方式耗时17.58s
+
+三、测试json.Marshal(简单操作), 协程池毫无优势
+1.通过协程池方式耗时4.71s
+2.通过普通方式耗时3.99s
 */
 func calcSum(value int) (ret int64) {
 	for index := 1; index <= value; index++ {
 		ret += int64(index)
 	}
 	return ret
+	// return int64(value) * int64(value)
 }
 
 func TestCalcSumWithPool(t *testing.T) {
@@ -65,6 +75,90 @@ func TestCalcSumWithoutPool(t *testing.T) {
 	}
 
 	t.Logf("total = %v", total)
+}
+
+type testPlayer struct {
+	Name   string
+	Age    int
+	Score  int64
+	Money  int64
+	Addr   string
+	School []byte
+}
+
+func initTestPlayer() (*testPlayer, []byte) {
+	p := &testPlayer{
+		Name:   "hello",
+		Age:    18,
+		Score:  100,
+		Money:  200,
+		Addr:   "127.0.0.1",
+		School: []byte("bei jing da xue & qing hua da da xue & zhe jiang da xue & fu dan da xue"),
+	}
+	data, _ := json.Marshal(p)
+	return p, data
+}
+
+func TestUnMarshalWithPool(t *testing.T) {
+	times := 10240000
+	pool := NewCoroutinePool()
+	p, d := initTestPlayer()
+	for i := 0; i < times; i++ {
+		pool.Run(func(i ...interface{}) {
+			if err := json.Unmarshal(i[0].([]byte), p); err != nil {
+				t.Errorf("反序列化错误, err = %v", err)
+			}
+		}, d)
+	}
+	tStart := time.Now()
+	for {
+		if pool.Empty() {
+			t.Logf("wait cost = %v ms", time.Since(tStart).Milliseconds())
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+}
+
+func TestUnMarshalWithoutPool(t *testing.T) {
+	times := 10240000
+	p, d := initTestPlayer()
+	for i := 0; i < times; i++ {
+		if err := json.Unmarshal(d, p); err != nil {
+			t.Errorf("反序列化错误, err = %v", err)
+		}
+	}
+}
+
+func TestMarshalWithPool(t *testing.T) {
+	times := 10240000
+	pool := NewCoroutinePool()
+	p, _ := initTestPlayer()
+	for i := 0; i < times; i++ {
+		pool.Run(func(i ...interface{}) {
+			if _, err := json.Marshal(i[0].(*testPlayer)); err != nil {
+				t.Errorf("反序列化错误, err = %v", err)
+			}
+		}, p)
+	}
+	tStart := time.Now()
+	for {
+		if pool.Empty() {
+			t.Logf("wait cost = %v ms", time.Since(tStart).Milliseconds())
+			break
+		}
+		time.Sleep(time.Millisecond)
+	}
+}
+
+func TestMarshalWithoutPool(t *testing.T) {
+	times := 10240000
+	p, _ := initTestPlayer()
+	for i := 0; i < times; i++ {
+		if _, err := json.Marshal(p); err != nil {
+			t.Errorf("序列化错误, err = %v", err)
+		}
+	}
 }
 
 func isSortSlice(arr []int64) bool {
