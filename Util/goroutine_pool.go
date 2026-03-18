1a82d21c74c006a692af9453207ec67d26fca1c7
hu <705811439@qq.com>
2026-03-18 16:39:08 +0800

    1. 协程库优化

--- Util/goroutine_pool.go
+++ Util/goroutine_pool.go
@@ -2,6 +2,7 @@ package Util
 
 import (
 	"container/list"
+	"math"
 	"runtime"
 	"sync"
 	"sync/atomic"
@@ -17,27 +18,34 @@ type Coroutine struct {
 	chDoTask     chan *task
 	chTaskFinish chan struct{}
 	taskList     *list.List
-	status       int32 // 状态: 0-空闲, 1-正在执行任务；仅 dispatch goroutine 访问
-	idxCount     int   // 有序执行的时候, 表示有多少标志的任务由他执行
+	idle         bool         // 当前调度链是否空闲 true-空闲 false-已有任务在执行或排队
+	idxCount     int          // 有序执行的时候, 表示有多少标志的任务由他执行
+	pending      atomic.Int64 // 当前待处理任务数(执行中 + 排队中)
 }
 
-func newCoroutine(group *sync.WaitGroup) *Coroutine {
+func newCoroutine() *Coroutine {
 	ret := &Coroutine{
 		chNewTask:    make(chan *task),
 		chDoTask:     make(chan *task),
 		chTaskFinish: make(chan struct{}),
 		taskList:     list.New(),
+		idle:         true,
 	}
 	go ret.dispatch()
-	go ret.doTask(group)
+	go ret.doTask()
 	return ret
 }
 
-func (c *Coroutine) doTask(group *sync.WaitGroup) {
+func (c *Coroutine) addTask(fun func(...interface{}), args ...interface{}) {
+	c.pending.Add(1)
+	c.chNewTask <- &task{fun: fun, args: args}
+}
+
+func (c *Coroutine) doTask() {
 	for {
 		task := <-c.chDoTask
 		task.fun(task.args...)
-		group.Done()
+		c.pending.Add(-1)
 		c.chTaskFinish <- struct{}{}
 	}
 }
@@ -48,38 +56,37 @@ func (c *Coroutine) dispatch() bool {
 		// 新任务需要执行
 		case newTask := <-c.chNewTask:
 			// 如果执行协程空闲，则直接通知执行协程执行任务；否则放入队列中。
-			if c.status == 0 {
-				c.status = 1
+			if c.idle {
+				c.idle = false
 				c.chDoTask <- newTask
 			} else {
 				c.taskList.PushBack(newTask)
 			}
 		// 工作协程有空闲
 		case <-c.chTaskFinish:
-			// 如果缓存队列有任务，则取出任务，通知执行协程执行任务；否则设置状态为空闲
+			// 如果缓存队列有任务，则取出任务，通知执行协程执行任务；否则说明当前协程已空闲
 			if front := c.taskList.Front(); front != nil {
 				c.taskList.Remove(front)
 				c.chDoTask <- front.Value.(*task)
 			} else {
-				c.status = 0
+				c.idle = true
 			}
 		}
 	}
 }
 
 type CoroutinePool struct {
-	pool           []*Coroutine // 协程列表
-	idxCoroutineId sync.Map     // map[idx]*Coroutine idx 对应的协程, 保证有序执行(RunOrder)时, 同一个idx由同一个协程执行, 保证执行顺序
-	next           uint64       // 用于实现轮询的 atomic 自增索引
-	wait           sync.WaitGroup
-	mutex          sync.Mutex // 保护 getOrderPool 中 idxCoroutineId / idxCount 的并发访问
+	pool           []*Coroutine  // 协程列表
+	idxCoroutineId sync.Map      // map[idx]*Coroutine idx 对应的协程, 保证有序执行(RunOrder)时, 同一个idx由同一个协程执行, 保证执行顺序
+	next           atomic.Uint64 // 用于实现轮询的 atomic 自增索引
+	mutex          sync.Mutex    // 保护 getOrderPool 中 idxCoroutineId / idxCount 的并发访问
 }
 
 func NewCoroutinePool() *CoroutinePool {
 	count := runtime.NumCPU()
 	ret := &CoroutinePool{}
 	for i := 0; i < count; i++ {
-		ret.pool = append(ret.pool, newCoroutine(&ret.wait))
+		ret.pool = append(ret.pool, newCoroutine())
 	}
 	return ret
 }
@@ -102,8 +109,7 @@ pool.RunOrder(10086, func(i ...interface{}) {
 可以保证hello world A 比 hello world B 先执行; hello world C 比 hello world D 先执行。但是hello world A 和 hello world C 会随机执行
 */
 func (c *CoroutinePool) RunOrder(idx int64, fun func(...interface{}), args ...interface{}) {
-	c.wait.Add(1)
-	c.getOrderPool(idx).chNewTask <- &task{fun: fun, args: args}
+	c.getOrderPool(idx).addTask(fun, args...)
 }
 
 // Run 无序执行
@@ -124,24 +130,24 @@ pool.Run(func(i ...interface{}) {
 这里虽然顺序写的E,F,G,H, 但是实际执行顺序不一定, 因为4个任务会被分配到不同的协程,各协程间独立, 不保证顺序
 */
 func (c *CoroutinePool) Run(fun func(...interface{}), args ...interface{}) {
-	c.wait.Add(1)
-	idx := atomic.AddUint64(&c.next, 1) % uint64(len(c.pool))
-	c.pool[idx].chNewTask <- &task{fun: fun, args: args}
-}
-
-func (c *CoroutinePool) Wait() {
-	c.wait.Wait()
+	pool := c.pool[c.next.Add(1)%uint64(len(c.pool))]
+	if pool.pending.Load() >= 5 {
+		c.getBestPool().addTask(fun, args...)
+	} else {
+		pool.addTask(fun, args...)
+	}
 }
 
 func (c *CoroutinePool) getOrderPool(idx int64) *Coroutine {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
+	// 锁内再检查一次
 	if v, ok := c.idxCoroutineId.Load(idx); ok {
 		return v.(*Coroutine)
 	}
-	// 新建协程并更新映射
-	minCount := int(^uint(0) >> 1) // 最大整数
+
+	minCount := math.MaxInt
 	var ret *Coroutine
 	for _, v := range c.pool {
 		if v.idxCount < minCount {
@@ -153,8 +159,19 @@ func (c *CoroutinePool) getOrderPool(idx int64) *Coroutine {
 		}
 	}
 	if ret != nil {
-		ret.idxCount += 1
+		ret.idxCount++
 		c.idxCoroutineId.Store(idx, ret)
+	}
+	return ret
+}
+
+func (c *CoroutinePool) getBestPool() (ret *Coroutine) {
+	minTaskCount := int64(math.MaxInt64)
+	for _, p := range c.pool {
+		if taskCount := p.pending.Load(); taskCount < minTaskCount {
+			minTaskCount = taskCount
+			ret = p
+		}
 	}
 	return ret
 }
